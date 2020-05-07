6e113180314c783a5406130b1b45d9109cec4734
Udara Senanayake <usenana94@gmail.com>
2020-05-07 22:39:33 +0530

    Initial commit

---
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -0,0 +1,39 @@
+package com.example.websocketdemo.controller;
+
+import com.example.websocketdemo.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by rajeevkumarsingh on 24/07/17.
+ */
+@Controller
+public class ChatController {
+	
+//	final String sendTo = "/topic/public";
+
+    @MessageMapping("/chat.sendMessage/{chatId}")
+    @SendTo("/topic/{chatId}")
+//	@MessageMapping("/chat.sendMessage")
+//	@SendTo("/topic")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser/{chatId}")
+    @SendTo("/topic/{chatId}")
+//	@MessageMapping("/chat.addUser")
+//	@SendTo("/topic")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+//        System.out.println(chatMessage.getChatId());
+        headerAccessor.getSessionAttributes().put("chatId", chatMessage.getChatId());
+        return chatMessage;
+    }
+
+}
